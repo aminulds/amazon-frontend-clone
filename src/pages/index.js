f05b55b24d6ad5a9fc40915a9eef98f442419c9a
Aminul Islam <aminulds61@gmail.com>
2021-12-04 01:58:51 +0600

    Banner done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,21 @@
 import Head from "next/head";
+import Banner from '../components/Banner';
 import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
-        <title>Amazon Frontend Clone</title>
+        <title>Amazon Frontend Clone with Nextjs</title>
       </Head>
 
-      {/* Header */}
       <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+
+        {/* Product feeds */}
+      </main>
     </div>
   );
 }
