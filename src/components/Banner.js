cf1fd716d9841ab2ef90ab08d53d4fb2fa796361
Aminul Islam <aminulds61@gmail.com>
2021-12-04 10:31:52 +0600

    Product feed done

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,18 +10,19 @@ function Banner() {
         infiniteLoop
         showStatus={false}
         showIndicators={false}
+        showThumbs={false}
         interval={5000}
       >
-        <div>
-          <img loading="lazy" src="https://links.papareact.com/gi1" alt="" />
-        </div>
-
         <div>
           <img loading="lazy" src="https://links.papareact.com/6ff" alt="" />
         </div>
 
         <div>
           <img loading="lazy" src="https://links.papareact.com/7ma" alt="" />
+        </div>
+
+        <div>
+          <img loading="lazy" src="https://links.papareact.com/gi1" alt="" />
         </div>
       </Carousel>
     </div>
